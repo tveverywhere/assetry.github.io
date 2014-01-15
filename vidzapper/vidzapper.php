e404da7182d39db75b1ad7e7334fba586b1dc695
Anuj Pandey <a@e10.in>
2014-01-15 16:06:25 +0530

    Removed Bad Credentials

--- vidzapper/vidzapper.php
+++ vidzapper/vidzapper.php
@@ -2,9 +2,9 @@
 require_once("vidzapper.api.php");
 
 $vidzapper = new VidZapper(array(
-	'appId' => "C0E45681-F940-4F83-9E5F-D8B7AFEC63FD",
-	'secret' => "03rFGCWNMDIGXSCXHSESPHZIENW",
-    'api'=>"https://assetry.vzconsole.com/api/",
+	'appId' => "",
+	'secret' => "",
+    'api'=>"",
     'cache'=>30,
     //'debug'=>true
 )); 
